fd03fcd60a7a018f72fa5772193803b35c3cb757
Sarah Deitke <sarah.deitke@duolingo.com>
2024-12-01 12:58:43 -0500

    part 1

--- AdventOfCode2024/Sources/Day1/Day1Solution.swift
+++ AdventOfCode2024/Sources/Day1/Day1Solution.swift
@@ -14,7 +14,30 @@ class Day1Solution {
         let fileURL = Bundle.main.url(forResource: "Day1Input", withExtension: "txt")
         let content = try! String(contentsOf: fileURL!, encoding: .utf8)
         let lines = content.components(separatedBy: .newlines)
-        print(lines[0])
+        
+        /// create empty lists for each row
+        var list1 = [Int]()
+        var list2 = [Int]()
 
+        for i in 0..<lines.count {
+            /// split each line by whitespace
+            let line = lines[i].components(separatedBy: "   ")
+            list1.append(Int(line[0])!)
+            list2.append(Int(line[1])!)
+        }
+        
+        /// sort each list in ascending order
+        list1.sort()
+        list2.sort()
+        
+        /// sum total
+        var total = 0;
+        
+        for i in 0..<list1.count {
+            /// compute absolute value between list1[i] and list2[i]
+            total += abs(list1[i] - list2[i])
+        }
+        
+        print(total)
     }
 }

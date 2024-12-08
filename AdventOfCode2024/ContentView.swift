407c25737795851b6d918e22239485b99f08596a
Sarah Deitke <sarah.deitke@duolingo.com>
2024-12-07 20:23:55 -0500

    day 7 part 1

--- AdventOfCode2024/ContentView.swift
+++ AdventOfCode2024/ContentView.swift
@@ -17,7 +17,7 @@ struct ContentView: View {
         }
         .padding()
         .onAppear {
-            Day6Solution().part2();
+            Day7Solution().part1();
         }
     }
 }
---
+++ AdventOfCode2024/Sources/Day1/Day7Solution.swift
@@ -0,0 +1,43 @@
+//
+//  Day7Solution.swift
+//  AdventOfCode2024
+//
+//  Created by Sarah Deitke on 12/7/24.
+//
+import Foundation
+
+public class Day7Solution {
+    func part1() {
+        let lines = Helpers().readFile(fileName: "Day7Input")
+        
+        var sum = 0
+        
+        for line in lines {
+            let equation = line.components(separatedBy: ":")
+            let inputs = equation[1].trimmingCharacters(in: .whitespaces).components(separatedBy: " ").map { Int($0)! }
+            sum += evaluateEquation(total: Int(equation[0])!, inputs: inputs)
+        }
+        
+        print(sum)
+    }
+    
+    func evaluateEquation(total: Int, inputs: [Int]) -> Int {
+        var options = [Int]()
+        options.append(inputs[0])
+        for i in 1..<inputs.count {
+            var newOptions = [Int]()
+            for option in options {
+                newOptions.append(option + inputs[i])
+                newOptions.append(option * inputs[i])
+            }
+            options = newOptions
+        }
+        
+        for option in options {
+            if option == total {
+                return total
+            }
+        }
+        return 0
+    }
+}
